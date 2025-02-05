c610a5ac1e8e9bac72325a031c751d64544b811b
Kirill Malina <malina.kirill@gmail.com>
2025-02-05 14:17:19 +0300

    update confirmPayment arguments type

--- src/types/ConfirmationPaymentParams.ts
+++ src/types/ConfirmationPaymentParams.ts
@@ -3,4 +3,6 @@ import type { PaymentMethodTypesEnum } from '.';
 export interface ConfirmationPaymentParams {
   confirmationUrl: string;
   paymentMethodType: PaymentMethodTypesEnum;
+  clientApplicationKey: string;
+  shopId: string
 }
